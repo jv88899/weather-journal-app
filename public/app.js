91e5ed439b53e1a647847d031b7da3cdceed8285
jv88899 <jv88899@gmail.com>
2020-03-14 12:10:21 -0500

    created global variables

--- public/app.js
+++ public/app.js
@@ -1,1 +1,8 @@
-// GET function
+// Global Variables
+const key = 'ddb32c28535519ff8e479f5099a03619'
+const zipHolder = document.querySelector('#zip')
+const feelingsHolder = document.querySelector('#feelings')
+const generateButton = document.querySelector('#generate')
+const dateHolder = document.querySelector('#date')
+const tempHolder = document.querySelector('#temp')
+const contentHolder = document.querySelector('#content')
