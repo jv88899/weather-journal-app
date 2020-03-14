4265af1b1e05f97622cbbe54deec978caaa1dc46
jv88899 <jv88899@gmail.com>
2020-03-14 12:11:59 -0500

    created getAllEntries function

--- public/app.js
+++ public/app.js
@@ -6,3 +6,16 @@ const generateButton = document.querySelector('#generate')
 const dateHolder = document.querySelector('#date')
 const tempHolder = document.querySelector('#temp')
 const contentHolder = document.querySelector('#content')
+
+// Async GET
+const getAllEntries = async () => {
+    const response = await fetch('/getAllEntries')
+    const body = await response.json()
+
+    if (response.status !== 200) {
+        throw Error(body.message)
+    }
+
+    console.log('body', body)
+    return body
+}
